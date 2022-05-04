18ef4e20693355db415c5c3f78f3f2a25feeba5d
FOXDOZE\Jack <jackfox@disroot.org>
2022-05-04 18:05:06 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
 
-function App() {
+
+const App = () => {
+
+function photosSelected() {
+  const files = document.querySelector("[type=file]").files;
+  console.log(files)
+}
+
+function submitFiles(event) {
+    const url = "https://api.cloudinary.com/v1_1/jandjtravels/image/upload";
+    console.log("url1" + url);
+    event.preventDefault();
+
+    const files = document.querySelector("[type=file]").files;
+    const formData = new FormData();
+
+    console.log("form Data 1" + formData)
+
+    for (let i = 0; i < files.length; i++) {
+      let file = files[i];
+      
+      formData.append("file", file);
+      formData.append("upload_preset", "stkv5cah");
+      formData.append("cloud_name", "jandjtravels");
+      console.log("form Data 2" + formData);
+      console.log("url2" + url);
+
+      fetch("https://api.cloudinary.com/v1_1/jandjtravels/image/upload", {
+        method: "POST",
+        body: formData
+      })
+        .then((response) => {
+          return response.text();
+        })
+        // .then((data) => {
+        //   document.getElementById("data").innerHTML += data;
+        // })
+        ;
+    }
+
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <form method="post" encType="multipart/form-data">
+          <input type="file" name="files[]" multiple  onChange={photosSelected}/>
+          <input type="submit" value="Upload Files" name="submit" onSubmit={submitFiles} />
+        </form>
+      </div>
+      <div>
+        <p id="data"></p>
+
+      </div>
+    </>
   );
 }
 
